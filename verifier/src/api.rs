1398da0d9cde35eb3ddec45e69c9f9f872af2c5e
Jakub Beránek <berykubik@gmail.com>
2024-10-17 13:23:32 +0200

    Update verifier script
    Add various normalizations to make it easier to check the contents of the survey.

--- verifier/src/api.rs
+++ verifier/src/api.rs
@@ -1,7 +1,9 @@
+use regex::Regex;
 use reqwest::blocking::Client as Reqwest;
 use serde::Deserialize;
-
+use std::borrow::Cow;
 use std::error::Error;
+use std::sync::LazyLock;
 
 pub struct Client {
     username: String,
@@ -127,19 +129,16 @@ pub struct ChoiceList {
 }
 
 impl ChoiceList {
-    pub fn as_strs(&self) -> impl Iterator<Item = &str> {
-        self.choices.iter().map(|c| c.label.as_str())
+    pub fn as_strs(&self) -> impl Iterator<Item = String> + '_ {
+        self.choices
+            .iter()
+            .map(|c| normalize_surveyhero_text(c.label.as_str()))
     }
 
-    pub fn mismatched_answers<'a>(&'a self, answers: &'a [&str]) -> Vec<(&'a str, &'a str)> {
+    pub fn mismatched_answers<'a>(&'a self, answers: &'a [&str]) -> Vec<(String, Cow<str>)> {
         self.as_strs()
-            .zip(answers.iter().map(|s| *s))
-            .filter(|(s1, s2)| {
-                if *s2 == "Other (open response)" {
-                    return *s1 != "Other";
-                }
-                s1 != s2
-            })
+            .zip(answers.iter().map(|s| normalize_markdown_text(s)))
+            .filter(|(s1, s2)| s1 != s2)
             .collect()
     }
 }
@@ -151,24 +150,28 @@ pub struct ChoiceTable {
 }
 
 impl ChoiceTable {
-    pub fn column_strs(&self) -> impl Iterator<Item = &str> {
-        self.choices.iter().map(|c| c.label.as_str())
+    pub fn column_strs(&self) -> impl Iterator<Item = String> + '_ {
+        self.choices
+            .iter()
+            .map(|c| normalize_surveyhero_text(c.label.as_str()))
     }
 
-    pub fn rows_strs(&self) -> impl Iterator<Item = &str> {
-        self.rows.iter().map(|c| c.label.as_str())
+    pub fn rows_strs(&self) -> impl Iterator<Item = String> + '_ {
+        self.rows
+            .iter()
+            .map(|c| normalize_surveyhero_text(c.label.as_str()))
     }
 
-    pub fn mismatched_rows<'a>(&'a self, labels: &[&'a str]) -> Vec<(&'a str, &'a str)> {
+    pub fn mismatched_rows<'a>(&'a self, labels: &[&'a str]) -> Vec<(String, Cow<str>)> {
         self.rows_strs()
-            .zip(labels.iter().map(|s| *s))
+            .zip(labels.iter().map(|s| normalize_markdown_text(s)))
             .filter(|(s1, s2)| s1 != s2)
             .collect()
     }
 
-    pub fn mismatched_columns<'a>(&'a self, choices: &'a [&str]) -> Vec<(&'a str, &'a str)> {
+    pub fn mismatched_columns<'a>(&'a self, choices: &'a [&str]) -> Vec<(String, Cow<str>)> {
         self.column_strs()
-            .zip(choices.iter().map(|s| *s))
+            .zip(choices.iter().map(|s| normalize_markdown_text(s)))
             .filter(|(s1, s2)| s1 != s2)
             .collect()
     }
@@ -197,4 +200,31 @@ pub struct Surveys {
 pub struct Survey {
     pub survey_id: usize,
     pub title: String,
+}
+
+fn normalize_surveyhero_text(text: &str) -> String {
+    static LINK_REGEX: LazyLock<Regex> =
+        LazyLock::new(|| Regex::new(r#"<a href="(?<link>.*?)".*?>(?<text>.*?)</a>"#).unwrap());
+
+    let text = text
+        .replace("&amp;", "&")
+        .replace("&nbsp;", " ")
+        .replace("&lt;", "<");
+
+    // Replace <a href="$link" ...>$text</a> with [$text]($link)
+    LINK_REGEX.replace_all(&text, "[$text]($link)").to_string()
+}
+
+fn normalize_markdown_text(text: &str) -> Cow<str> {
+    static ITALICS_REGEX: LazyLock<Regex> =
+        LazyLock::new(|| Regex::new(r"(\*(?<text>\w+)\*)").unwrap());
+
+    // Remove (open response) from the end of the Markdown text
+    let answer = text
+        .strip_suffix("(open response)")
+        .map(|t| t.trim_end())
+        .unwrap_or(text);
+
+    // Replace *foo* with <em>foo</em>
+    ITALICS_REGEX.replace_all(answer, "<em>$text</em>")
 }
